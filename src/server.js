12b3c9db227a409b54e014bb9ce9e6d01955d142
93minki <alsrl0023@naver.com>
2022-02-04 14:32:22 +0900

    test

--- src/server.js
+++ src/server.js
@@ -24,15 +24,8 @@ app.use(express.json());
 app.use((req, res, next) => {
   res.header("Cross-Origin-Embedder-Policy", "require-corp");
   res.header("Cross-Origin-Opener-Policy", "same-origin");
-  next();
-});
-
-app.use((req, res, next) => {
-  res.setHeader("Access-Control-Allow-Origin", "*");
-  res.header(
-    "Access-Control-Allow-Headers",
-    "Origin, X-Requested-With, Content-Type, Accept"
-  );
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "*");
   next();
 });
 
