33212a114dc9c5914267132615bd71945da79c39
93minki <alsrl0023@naver.com>
2022-02-04 15:01:28 +0900

    videoplayer.js

--- src/client/js/videoPlayer.js
+++ src/client/js/videoPlayer.js
@@ -123,6 +123,7 @@ playBtn.addEventListener("click", handlePlayClick);
 muteBtn.addEventListener("click", handleMuteClick);
 volumeRange.addEventListener("input", handleVolumeChange);
 video.addEventListener("canplay", handleLoadedMetadata);
+handleLoadedMetadata();
 video.addEventListener("timeupdate", handleTimeUpdate);
 video.addEventListener("click", handleVideoClick);
 video.addEventListener("ended", handleEnded);
